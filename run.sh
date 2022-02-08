a5b016b37171943a93fac2420074a1db3648ffd8
Marco Tulio Dal-Col dos Santos <tuliodalcol@gmail.com>
2022-02-08 02:59:57 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ python3 -m venv env
 echo 'Activating Environment...'
 . env/bin/activate
 echo 'Installing packages...'
-pip3 install --upgrade pip
+pip3 -q install --upgrade pip
 pip3 -q install -r requirements.txt
 echo 'Pachages Installed!!!'
 echo 'Run unittest'
