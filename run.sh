51d48bea5de8d35ce93dd7127cb0f612ffe6d2b0
Marco Tulio Dal-Col dos Santos <tuliodalcol@gmail.com>
2022-02-08 02:58:37 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@ echo 'Activating Environment...'
 . env/bin/activate
 echo 'Installing packages...'
 pip3 install --upgrade pip
-pip3 install -r requirements.txt
+pip3 -q install -r requirements.txt
 echo 'Pachages Installed!!!'
 echo 'Run unittest'
 python3 -m unittest tests/test_extract.py
